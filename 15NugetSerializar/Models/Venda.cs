83963a15fdd82202a73c92175c8aab938df7e49d
Erasmo Ribeiro Bezerra <erasmo.ads.tech@gmail.com>
2025-08-11 15:27:45 -0300

    incluindo 15NugetSerializer

---
+++ 15NugetSerializar/Models/Venda.cs
@@ -0,0 +1,29 @@
+using Newtonsoft.Json;
+
+// JsonProperty -> Mapeia a propriedade da classe para o nome especificado no JSON
+
+namespace _15NugetSerializar.Models
+{
+    public class Venda
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("nome_produto")]
+        public string Produto { get; set; }
+
+        [JsonProperty("preco")]
+        public decimal Preco { get; set; }
+
+        [JsonProperty("data_venda")]
+        public DateTime DataVenda { get; set; }
+
+        public Venda(int id, string produto, decimal preco, DateTime dataVenda)
+        {
+            Id = id;
+            Produto = produto;
+            Preco = preco;
+            DataVenda = dataVenda;
+        }
+    }
+}
---
+++ 15NugetSerializar/Program.cs
@@ -0,0 +1,33 @@
+﻿using _15NugetSerializar.Models;
+using Newtonsoft.Json;
+
+List<Venda> listaVendas = new List<Venda>();
+DateTime dataAtual = DateTime.Now;
+
+Venda v1 = new Venda(1, "Meterial de Escritório", 25.00M, dataAtual);
+Venda v2 = new Venda(2, "Licença de Software", 110.00M, dataAtual);
+
+listaVendas.Add(v1);
+listaVendas.Add(v2);
+
+// JsonConvert -> biblioteca do Newtonsoft para conversão entre objetos .NET e JSON
+// Serialização -> converte a lista de vendas em uma string JSON
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+File.WriteAllText("Arquivos/vendas-serialize.json", serializado);
+
+Console.WriteLine("Serializando Objeto em JSON: ");
+Console.WriteLine(serializado);
+Console.WriteLine("-------------------------------------------------------");
+
+string conteudoArquivo = File.ReadAllText("Arquivos/vendas-deserialize.json");
+
+// Deserialização -> converte a string JSON de volta para uma lista de objetos Venda
+List<Venda> listaVendas2 = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+
+Console.WriteLine("Deserializando JSON em Objeto: ");
+foreach (Venda venda in listaVendas2)
+{
+    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}" +
+                        $"Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("dd/MM/yyyy HH:mm")}");
+}
