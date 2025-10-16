697d02fd7d7e129eb69c366e8ab37362483c009c
Erasmo Ribeiro Bezerra <erasmo.ads.tech@gmail.com>
2025-10-16 11:03:24 -0300

    incluindo Heap, Stack e Garbage Collection

---
+++ 20StackHeapGarbageCollection/Models/Pessoa.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _20StackHeapGarbageCollection.Models
+{
+    public class Pessoa
+    {
+        private string _nome;
+        //private int _idade;
+
+        public string Nome
+        {
+            // Body Expressions -> abstrai as chaves e return
+            get => _nome;
+            // Validação de entrada
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Nome não pode ser vazio ou nulo.");
+                }
+                _nome = value;
+            }
+        }
+
+        // public int Idade
+        // {
+        //     get => _idade;
+        //     set
+        //     {
+        //         if (value < 0)
+        //         {
+        //             throw new ArgumentException("Idade não pode ser negativa.");
+        //         }
+        //         _idade = value;
+        //     }
+        // }
+
+        public string Sobrenome { get; set; }
+
+        // Propriedade somente leitura que combina Nome e Sobrenome
+        public string NomeCompleto { get => $"{Nome} {Sobrenome}"; }
+
+        // Construtor padrão sem parâmetros
+        public Pessoa() { }
+
+        // Construtor padrão
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+
+        // // Metodo para apresentar informações da pessoa
+        // public void Apresentar()
+        // {
+        //     Console.WriteLine($"Nome: {NomeCompleto}, Idade: {Idade}");
+        // }
+
+
+    }
---
+++ 20StackHeapGarbageCollection/Program.cs
@@ -0,0 +1,38 @@
+﻿using _20StackHeapGarbageCollection.Models;
+
+// Alocação de memória
+
+// Vamos entender os dois tipos de memória existentes no C#, tipos de valores e tipos de referência.
+// O .NET trabalha com dois tipos de memória, onde um armazena dados estáticos e o outro armazena dados dinâmicos.
+
+// Stack -> Armazena tipos de valores (int, double, bool, struct, enum)
+//       -> O tipo de valor armazena dados estáticos e não complexos
+
+// Heap -> Armazena tipos de referência (class, array, interface, string, delegate)
+//      -> O tipo de referência armazena dados dinâmicos e complexos, 
+
+// Garbage Collector (GC) -> A limpeza da memória Heap não é feita de maneira tradicional, sendo assim, ela depende do Garbage Collector (GC)
+//                             para fazer a limpeza de memória, liberando espaço que não está mais sendo utilizado.
+
+
+// Tipos de valor e referência
+
+// Tipo de valor: Uma variável de um tipo de valor contém uma instância do tipo.
+// Quando você copia a variável, você copia a instância.
+int a = 10;
+
+int b = a;
+b = 60;
+
+Console.WriteLine($"Valor de A: {a}");
+Console.WriteLine($"Valor de B: {b}");
+
+// Tipo de referência: Uma variável de um tipo de referência contém uma referência a uma instância do tipo .
+// Quando você copia a variável, você copia a referência, não a instância.
+Pessoa p1 = new Pessoa(nome: "Leonardo", sobrenome: "Buta");
+
+Pessoa p2 = p1;
+p2.Nome = "Vinicius";
+
+Console.WriteLine($"Nome da pessoa p1: {p1.NomeCompleto}");
+Console.WriteLine($"Nome da pessoa p2: {p2.NomeCompleto}");
